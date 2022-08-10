69393f0c5bf34b03800e61a9a439d51f76f2f2fc
andrew-bolin <andrew.bolin@csiro.au>
2022-08-10 14:50:23 +1000

    PERENTIE-1450 Add experimental command arguments

--- src/ska_low_cbf_sw_cnic/__main__.py
+++ src/ska_low_cbf_sw_cnic/__main__.py
@@ -9,9 +9,12 @@
 ska-low-cbf-sw-cnic module main
 (executed via `python3 -m ska_low_cbf_sw_cnic`)
 """
+import argparse
+import textwrap
 
 from ska_low_cbf_fpga.fpga_cmdline import FpgaCmdline
 
+import ska_low_cbf_sw_cnic.monitor as monitor
 from ska_low_cbf_sw_cnic.cnic_fpga import CnicFpga
 
 
@@ -33,9 +36,54 @@ class CnicCmdline(FpgaCmdline):
             default=24,
         )
 
+        # TODO move command handling to base class
+        #  (could be automatic by discovering user methods of the Personality?)
+
+        # this setting lets us have multi-line help
+        self.parser.formatter_class = argparse.RawTextHelpFormatter
+        self.parser.add_argument(
+            "command",
+            nargs="*",
+            type=str,
+            help=textwrap.dedent(
+                """
+                EXPERIMENTAL COMMAND INTERFACE
+                Available commands:
+                \tmonitor
+                \ttx <filename>
+                \trx <filename> <packet size> <n packets>
+                """
+            ),
+        )
+
     def set_personality_args(self):
         """Add CnicFpga personality extra arguments"""
         self.personality_args["ptp_domain"] = self.args.ptp_domain
+
+    # TODO move command handling to base class?
+    def run(self):
+        super().run()
+        command = self.args.command
+        fpga = self.fpgas[self.args.cards[0]]
+        if command:
+            base_cmd = str.lower(command[0])
+            if base_cmd == "monitor":
+                monitor.display_status_forever(fpga)
+            elif base_cmd == "tx":
+                assert len(command) == 2, "use 'tx <filename>'"
+                print(f"Transmitting {command[1]}")
+                # TODO - other transmit parameters...
+                fpga.transmit_pcap(command[1])
+            elif base_cmd == "rx":
+                assert (
+                    len(command) == 4
+                ), "use 'rx <filename> <packet size> <n packets>'"
+                print(f"Writing to {command[1]}")
+                print(f"{command[3]} packets, each of size {command[2]}B")
+                fpga.receive_pcap(command[1], int(command[2]), int(command[3]))
+
+            else:
+                raise NotImplementedError(f"No such command {command[0]}")
 
 
 def main():
