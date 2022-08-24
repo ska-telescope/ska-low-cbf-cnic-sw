65729a610b14ecf319cba55987df3231c67cd6c1
andrew-bolin <andrew.bolin@csiro.au>
2022-08-24 17:35:58 +1000

    PERENTIE-1315 Bugfix and other mods to compare_pcap

--- src/ska_low_cbf_sw_cnic/compare_pcap.py
+++ src/ska_low_cbf_sw_cnic/compare_pcap.py
@@ -4,24 +4,35 @@ import json
 from scapy.all import UDP, rdpcap
 
 
-def compare_n_packets(max_packets, packets, packets_capture, dport):
+def compare_n_packets(
+    max_packets, packets, packets_capture, dport
+) -> (dict, int):
+    """
+    Compare a number of packets
+    :param max_packets: maximum number of packets to compare
+    :param packets: original source set of packets
+    :param packets_capture: actual captured set of packets
+    :param dport: destination port of interest (filter applied to `packets_capture`)
+    :return: dict listing differences (key: packet index excluding dport mismatches),
+    int number of packets compared
+    """
 
     if len(packets) != len(packets_capture):
         print(
             "WARNING! Capture files not same length. "
             f"{len(packets)} packets vs {len(packets_capture)} packets"
         )
-    index_with_rebase = 0
+    index = 0
     results = {}
     for packet in packets_capture:
         if packet.haslayer(UDP):
             if packet.dport == dport:
-                if packet != packets[index_with_rebase]:
-                    results[index_with_rebase] = "Not matching"
-                index_with_rebase = index_with_rebase + 1
-                if index_with_rebase > max_packets:
-                    return results
-    return results
+                if packet != packets[index]:
+                    results[index] = "Not matching"
+                index = index + 1
+                if index > max_packets:
+                    break
+    return results, index
 
 
 def main():
@@ -30,9 +41,9 @@ def main():
     )
     argparser.add_argument(
         "input",
-        type=argparse.FileType("r"),
+        type=argparse.FileType("rb"),
         nargs=2,
-        help="Input pcap trace files",
+        help="Input pcap trace files. Second file (only) is filtered by dport.",
     )
     argparser.add_argument(
         "--packets",
@@ -55,11 +66,14 @@ def main():
 
     args = argparser.parse_args()
 
-    differences = compare_n_packets(
-        args.packets, rdpcap(args.input[0].name), rdpcap(args.input[1].name)
-    )
+    print(f"Loading {args.input[0].name}")
+    a = rdpcap(args.input[0])
+    print(f"Loading {args.input[1].name}")
+    b = rdpcap(args.input[1])
+    print("Comparing...")
+    differences, n_comp = compare_n_packets(args.packets, a, b, args.dport)
     if len(differences) == 0:
-        print("Two files contain same packets")
+        print(f"Two files contain same packets ({n_comp} packets compared)")
     else:
         print("Files are different")
         args.report.write(json.dumps(differences))
